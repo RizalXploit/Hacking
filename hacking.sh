5963d31edbb34ec9c72da6bde6e9cb262fb6b076
RizalXploit <116887451+RizalXploit@users.noreply.github.com>
2022-10-28 18:45:22 +0700

    Add files via upload

---
+++ hacking.sh
@@ -0,0 +1,40 @@
+clear
+
+echo
+echo
+echo -e "\e[1;36m[•]       Author : A./cyfin         [•]"
+echo " [•]         Ft : Ka ./RizalXploit          [•]"
+echo " whatsapp : +6285390035894"
+echo
+echo
+echo
+echo -e "\e[1;36mpilihan Menu:"
+echo
+echo "1: Hack Satelit"
+echo "2: Hack kominfo"
+echo "3: Hack data presiden vladimir putin"
+echo
+read -p "pilih mana anjing!: " p
+
+ if [[] $p = "1" ]]
+ then
+ bash satelit.sh
+ fi
+ 
+ if [[ $p = "2" ]]
+ then
+ bash kominfo.sh
+ fi
+ 
+ if [[ $p = "3" ]]
+ then
+ bash data.sh
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
